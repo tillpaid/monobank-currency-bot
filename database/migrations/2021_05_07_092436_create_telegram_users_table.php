2272c691140de3b3541598818f50af67134e4c19
Alexandr Maiboroda <maiboroda@evergreens.in.ua>
2021-05-07 12:25:42 +0300

    Added telegram user model

---
+++ database/migrations/2021_05_07_092436_create_telegram_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTelegramUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telegram_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('chat_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telegram_users');
+    }
+}
