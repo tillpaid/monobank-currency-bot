97a4319dc1f44f10811226e50cbeb9e79e6c59ae
Alexandr Maiboroda <maiboroda@evergreens.in.ua>
2021-05-07 13:29:22 +0300

    Added relations to models

--- app/Models/CurrencyAccount.php
+++ app/Models/CurrencyAccount.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CurrencyAccount extends Model
 {
     use HasFactory;
+
+    public function telegramUser(): BelongsTo
+    {
+        return $this->belongsTo(TelegramUser::class);
+    }
 }
