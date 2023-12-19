841f1bcf57cfd758f4296cfc2a59503d263a2d12
Oleksandr Maiboroda <oleksandr.maiboroda@paysera.net>
2023-12-19 18:22:06 +0200

    Fixed tests

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -4,6 +4,7 @@ namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Artisan;
 
 trait CreatesApplication
 {
@@ -12,7 +13,13 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
+        $this->clearCache();
 
         return $app;
+    }
+
+    private function clearCache(): void
+    {
+        Artisan::call('cache:clear');
     }
 }
