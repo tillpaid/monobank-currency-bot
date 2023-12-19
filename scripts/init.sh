9d40129e689ed399ddb7bb75ddd9c2939ad55e19
Oleksandr Maiboroda <oleksandr.maiboroda@paysera.net>
2023-12-19 16:05:48 +0200

    Deleted scripts

--- scripts/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Monobank update"
-docker exec monobank_currency_bot_app php artisan monobank:update
-
-echo "Telegram set webhook"
-docker exec monobank_currency_bot_app php artisan telegram:set-webhook
-
